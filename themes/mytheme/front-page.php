0a1568a4ec5e379e29b8bd9e7dbf70ed1b513374
hollythedev <me@hollythedev.tech>
2017-03-15 01:19:09 -0700

    set up front-page php and starting to style indvidual font page divs

---
+++ themes/mytheme/front-page.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * The template for the front page.
+ *
+ * @package mytheme_Theme
+ */
+get_header(); ?>
+
+<div id= "home" class ="hero-banner">
+    <div class= "banner-container">   
+        <h2 class= "banner-container-title"> thanks for <span class= "banner-title-span"> dropping</span> by my site </h2>
+    </div>
+</div>
+
+<div id= "about" class= "about-section">
+    <h1 class= "about-title"> just a bit <br> about me</h1>
+    <p class= "about-description">Pok pok brooklyn assumenda, elit try-hard normcore subway tile gochujang anim trust fund tumblr thundercats skateboard pabst. Marfa minim labore, duis paleo delectus occaecat man bun biodiesel butcher cold-pressed sunt pok pok XOXO freegan. Echo park air plant tilde, occaecat pork belly squid typewriter actually cillum hella tote bag. Man braid you probably haven't heard of them church-key, synth tofu eu banjo chicharrones hoodie mlkshk shabby chic duis hammock butcher mumblecore.</p>
+</div>
+
+<div id= "portfolio" class= "portfolio">
+    <h1 class= "portfolio-title"> my portfolio </h1>
+    <div class= "portfolio-grid">
+
+    </div>
+    <a href=# class="portfolio-link read-more"> </a>
+</div>
+
+<div id= "skills" class= "skillset">
+    <h1 class= "skillset-title"> what i offer </h1>
+    <p class= "skillset-description"></p>
+    <a href=# class="skillset-download-resume"> </a>
+    <div class= "skillset-grid">
+</div>
+
+<div id= "contact" class= "contact-section">   
+    <div class= "contact-map"> </div>
+    <div class= "contact-form"> </div>
+    <div class= "contact-container">
+        <h1 class= "contact-title"> lets keep <br> in touch</h1>
+        <p class= "contact-description"> occaecat pork belly squid typewriter actually cillum hella tote bag. Man braid you probably haven't heard of them church-key, synth tofu eu banjo chicharrones hoodie mlkshk shabby </p>
+        <ul class= "contact-connect">
+            <li class= "github"> <i class="fa fa-github-alt" aria-hidden="true"></i> </li>
+            <li class= "linkedin"> <i class="fa fa-linkedin" aria-hidden="true"></i> </li>
+            <li class= "email"> <i class="fa fa-envelope" aria-hidden="true"></i> </li>
+            <li class= "youtube"> <i class="fa fa-youtube" aria-hidden="true"></i> </li>
+        </ul>
+    </div>
+</div>
